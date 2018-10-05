d90784e432905e8632090a13a94dbad311afd327
Shuu Terumi <nguyen.tran.forte1609@gmail.com>
2018-10-05 10:53:13 +0700

    Complete!

--- IndecisionApp/src/components/Options.js
+++ IndecisionApp/src/components/Options.js
@@ -1,41 +1,25 @@
 import React from 'react';
+import Option from './Option';
 
-const Options = (props) => {
-    return (
-        <div>
-            <button
-                onClick={props.handleDeleteOptions}
-            >
-                Remove All
-            </button>
-            <ul>
-                {
-                    props.options.map(option => {
-                        return <Option
-                            optionText={option}
-                            key={option}
-                            handleSingleDeleteOption={props.handleSingleDeleteOption}
-                        />
-                    })
-                }
-            </ul>
-        </div>
-    )
-}
-
-const Option = (props) => {
-    return (
-        <div>
-            {props.optionText}
-            <button
-                onClick={(e) => {
-                    props.handleSingleDeleteOption(props.optionText);
-                }}
-            >
-                Remove
-            </button>
-        </div>
-    )
-}
+const Options = (props) => (
+    <div>
+        <button
+            onClick={props.handleDeleteOptions}
+        >
+            Remove All
+        </button>
+        <ul>
+            {
+                props.options.map(option => {
+                    return <Option
+                        optionText={option}
+                        key={option}
+                        handleSingleDeleteOption={props.handleSingleDeleteOption}
+                    />
+                })
+            }
+        </ul>
+    </div>
+)
 
 export default Options;
