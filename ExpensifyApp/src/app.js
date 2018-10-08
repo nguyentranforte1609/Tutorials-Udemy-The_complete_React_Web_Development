ed1e06d20b94ff0cb288d1015dc084edbc87a8d9
Shuu Terumi <nguyen.tran.forte1609@gmail.com>
2018-10-08 09:20:15 +0700

    Inital Setup

---
+++ ExpensifyApp/src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss'
+
+const template = <p>Hello there (  '  v ')> It's me again!!</p>
+
+ReactDOM.render(template, document.getElementById('app'))
